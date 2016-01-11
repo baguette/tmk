d1dadfa474c27666892426ee92275b3ff593935d
Cory Burgett <cmburget@gmail.com>
2016-01-11 14:38:40 -0500

    Add wrapper script for clang-static-analyzer

---
+++ run-clang-scan.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Running bootstrap build"
+TMAKE_BUILD=$(./bootstrap.sh |grep gcc |grep 'tmake\.c')
+
+echo
+echo "Using build command: $TMAKE_BUILD"
+echo
+
+echo "Building tmake only with clang-analyzer"
+scan-build $@ -o clang-int $TMAKE_BUILD
+
+echo
+echo "Report generated at clang-int/index.html"
+echo
