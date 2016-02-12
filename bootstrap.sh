9ee69ebec33b78c9c7be070be9ae3bf1eae12294
Cory Burgett <cmburget@gmail.com>
2016-02-12 00:58:09 -0500

    Fix the bootstrap script.
    It previous wasn't working on a clean clone.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,13 @@
 set -e
 
 CC=${CC:-gcc}
-CFLAGS=${CFLAGS:- -ansi -Wall -Wextra -g -O2}
+CFLAGS=${CFLAGS:- -ansi -Wall -Wextra -g -O2 -fPIC}
 
 AR=${AR:-ar}
 RANLIB=${RANLIB:-ranlib}
 
-JIM_CFLAGS="-D_GNU_SOURCE -Wall -I. -g -O2 -fno-unwind-tables -fno-asynchronous-unwind-tables"
+JIM_CFLAGS="-D_GNU_SOURCE -Wall -I. -g -O2 -fPIC
+            -fno-unwind-tables -fno-asynchronous-unwind-tables"
 
 run() {
 	echo "$@"
@@ -42,6 +43,12 @@ fi
 
 echo "typedef $WORDTYPE WORD;" >> config.h
 
+JIM_WITH_EXT="binary tclprefix"
+JIM_WITHOUT_EXT="eventloop history load syslog"
+JIM_CONFIG='--disable-lineedit
+            --math
+            --disable-docs'
+
 JIM_CSRC="_load-static-exts.c jim-subcmd.c jim-interactive.c jim-format.c
          jim.c utf8.c jimregexp.c jim-aio.c jim-array.c jim-clock.c
          jim-exec.c jim-file.c jim-namespace.c jim-pack.c jim-package.c
@@ -57,33 +64,43 @@ JIM_STATIC_EXTS="aio array clock exec file namespace pack
                  binary glob nshelper oo stdlib tclcompat tree"
 
 # Build the bootstrap JimTcl interpreter
-(cd jimtcl/autosetup;
- $CC -o ../jimsh0 jimsh0.c 2>/dev/null)
+# Add a link to jimsh0 in the autosetup directory
+# so that the configure script doesn't build it again'
+( cd jimtcl/autosetup;
+  run $CC -o ../jimsh0 jimsh0.c 2>/dev/null
+  ln -s $(dirname ../jimsh0) jimsh0
+)
 
 # Build the real JimTcl interpreter and library
-(cd jimtcl;
- echo '#define JIM_EMBEDDED' > _load-static-exts.c;
- echo "./jimsh0 make-load-static-exts.tcl $JIM_STATIC_EXTS >> _load-static-exts.c";
- ./jimsh0 make-load-static-exts.tcl $JIM_STATIC_EXTS >> _load-static-exts.c
+( cd jimtcl;
 
- for tclfile in $JIM_TCLSRC; do
-   echo '#define JIM_EMBEDDED' > _$(rootname $tclfile).c;
-   echo "./jimsh0 make-c-ext.tcl $tclfile >> _$(rootname $tclfile).c"
-   ./jimsh0 make-c-ext.tcl $tclfile >> _$(rootname $tclfile).c;
- done;
+  echo ./configure $JIM_CONFIG --with-ext="$JIM_WITH_EXT" \
+                               --without-ext="$JIM_WITHOUT_EXT";
+  ./configure $JIM_CONFIG --with-ext="$JIM_WITH_EXT" \
+                          --without-ext="$JIM_WITHOUT_EXT";
+  echo '#define JIM_EMBEDDED' > _load-static-exts.c;
 
- JIM_OBJS=""
- for cfile in $JIM_CSRC; do
-   JIM_OBJS="$JIM_OBJS $(rootname $cfile).o";
-   run $CC $JIM_CFLAGS -o $(rootname $cfile).o -c $cfile;
- done;
+  echo "./jimsh0 make-load-static-exts.tcl $JIM_STATIC_EXTS >> _load-static-exts.c";
+  ./jimsh0 make-load-static-exts.tcl $JIM_STATIC_EXTS >> _load-static-exts.c;
 
- run $AR cr libjim.a $JIM_OBJS;
- run $RANLIB libjim.a
+  for tclfile in $JIM_TCLSRC; do
+    echo '#define JIM_EMBEDDED' > _$(rootname $tclfile).c;
+    echo "./jimsh0 make-c-ext.tcl $tclfile >> _$(rootname $tclfile).c";
+    ./jimsh0 make-c-ext.tcl $tclfile >> _$(rootname $tclfile).c;
+  done;
+
+  JIM_OBJS="";
+  for cfile in $JIM_CSRC; do
+    JIM_OBJS="$JIM_OBJS $(rootname $cfile).o";
+    run $CC $JIM_CFLAGS -o $(rootname $cfile).o -c $cfile;
+  done;
+
+  run $AR cq libjim.a $JIM_OBJS;
+  run $RANLIB libjim.a
 )
 
 # Build SQLite
-run $CC -o sqlite3.o -c jimtcl/sqlite3/sqlite3.c \
+run $CC -fPIC -o sqlite3.o -c jimtcl/sqlite3/sqlite3.c \
     -DSQLITE_OMIT_LOAD_EXTENSION=1 -DSQLITE_THREADSAFE=0 \
     -DSQLITE_DEFAULT_FILE_FORMAT=4 -DSQLITE_ENABLE_STAT3 \
     -DSQLITE_ENABLE_LOCKING_STYLE=0 -DSQLITE_OMIT_INCRBLOB
@@ -91,7 +108,7 @@ run $CC -o sqlite3.o -c jimtcl/sqlite3/sqlite3.c \
 # Build TMk
 C_SRC="tmake.c tm_crypto.c tm_target.c tm_update.c tm_core_cmds.c tm_ext_cmds.c"
 
-MAKE_C_EXT="jimtcl/jimsh jimtcl/make-c-ext.tcl tm_ext_cmds.tcl"
+MAKE_C_EXT="jimtcl/jimsh0 jimtcl/make-c-ext.tcl tm_ext_cmds.tcl"
 echo "$MAKE_C_EXT > tm_ext_cmds.c"
 echo "#define JIM_EMBEDDED" > tm_ext_cmds.c
 $MAKE_C_EXT >> tm_ext_cmds.c
@@ -99,6 +116,7 @@ $MAKE_C_EXT >> tm_ext_cmds.c
 TM_OPSYS=$(uname -s)
 TM_MACHINE_ARCH=$(uname -m)
 
-run $CC -o tmk $CFLAGS -Ijimtcl -Ijimtcl/sqlite3 \
+run $CC -o tmk $CFLAGS -Ijimtcl -Ijimtcl/sqlite3 -Ljimtcl \
         -DTM_OPSYS="\"$TM_OPSYS\"" -DTM_MACHINE_ARCH="\"$TM_MACHINE_ARCH\"" \
-        $C_SRC jimtcl/libjim.a sqlite3.o -lm
+        $C_SRC sqlite3.o -ljim -lm
+
